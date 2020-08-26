1045d06111dbaf4a8c8d1f045a79c20beb39cd0a
Graeme Norris <gnorris@shawnigan.ca>
2020-08-26 13:25:05 -0700

    users can now add weather to their list

--- src/index.js
+++ src/index.js
@@ -1,71 +1,17 @@
-const weatherView = (function() {
+import {weatherController} from './controller';
 
-    function render() {
-        console.log("weather added to the html");
-    }
+function weatherReport(city, temp, type) {
     return {
-        render: render
-    };
-}) ();
-
-const weatherController =(function() {
-    let weathers = [];
-    function getWeather(){
-        console.log("getting weather");
-        addWeather();
-    }
-    function addWeather() {
-        console.log("weathers.push(item)");
-    }
-
-    return {
-        getWeather: getWeather
-    };
-
-}) ();
-
-function weatherReport(city, description, time) {
-
-    const getWeather = () => {
-        console.log(`the city of ${city}, is going to be ${description} at ${time}`)
-    }
-
-
-    return {
-        getWeather: getWeather,
         city,
-        description,
-        time
+        type,
+        temp
     };
 
 }
 
-let b = document.createElement("button");
-b.innerHTML = "render weather";
+let b = document.getElementById('weather-button');
 b.onclick = function() {
-    weatherView.render();
-    weatherController.getWeather();
+    weatherController.getWeather(document.getElementById('city').value);
 }
-document.body.appendChild(b);
 
-
-
-
-/*let weather = new Vue({
-    el: "#list",
-    data: {
-        
-        items: [
-            {text: "noice", gender: "b"},
-            {text: "noice", gender: "g"},
-            {text: "noice", gender: "b"}
-        ],
-        methods: [
-            function sayItem() {
-                console.log(this.list);
-            }
-        ]
-
-    }
-
-})*/
+export {weatherReport}
